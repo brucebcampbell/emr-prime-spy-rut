cff639dc49cd09bee0252bf9fa5e03ae2b331bcc
Bruce Campbell <bruce.bishop.campbell@gmail.com>
2019-07-10 22:11:02 -0400

    dev

--- FinancialData_InsertPriceData.sql
+++ FinancialData_InsertPriceData.sql
@@ -30,4 +30,15 @@ from price_temp;
 
 
 
+insert into table '${hiveconf:basketTable}_extern'
+select 
+'${hiveconf:tickerName}' as tickerName,
+DateOfPrice,
+Open as Open,
+High as High,
+Low as Low,
+Close as Close,
+Volume as Volume,
+AdjClose as AdjClose 
+from price_temp;
 
