4c06297ca95b433871c84a90c66bd93b7477b2f8
Bruce Campbell <bruce.bishop.campbell@gmail.com>
2019-07-10 22:13:25 -0400

    dev

--- FinancialData_CreateDatabaseTables.sql
+++ FinancialData_CreateDatabaseTables.sql
@@ -29,3 +29,28 @@ Volume double,
 AdjClose double
 ) stored as orc tblproperties ("orc.compress"="NONE");
 
+drop TABLE if exists spy_price_extern;
+CREATE TABLE spy_price_extern  (
+DateOfPrice Date,
+Open double,
+High double,
+Low double,
+Close double,
+Volume double,
+AdjClose double
+) PARTITIONED BY (tickername string)
+ STORED AS PARQUET
+ LOCATION 's3n://aloidia-solutions/parquet-data/fintech/financial_data_external_spy_price';
+
+drop TABLE if exists rut_price_extern;
+CREATE TABLE rut_price_extern  (
+DateOfPrice Date,
+Open double,
+High double,
+Low double,
+Close double,
+Volume double,
+AdjClose double
+) PARTITIONED BY (tickername string) 
+ STORED AS PARQUET
+ LOCATION 's3n://aloidia-solutions/parquet-data/fintechfinancial_data_external_rut_price';
